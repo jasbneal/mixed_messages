53555f138ba08be29d839eae5598f78ad7903482
jasbneal <75139330+jasbneal@users.noreply.github.com>
2021-04-29 14:20:21 -0400

    messageComponents obj, removed arrays

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 /* Mixed Messages is a program that generates a random inspiration message 
 for a founder when it runs. */
 
-const inspo1 = ['"Always deliver more than expected." -Larry Page, co-founder at Google', 
-    '"Stay self-funded as long as possible." -Garrett Camp, co-founder Stumbleupon',
+const messageComponents = {
+    'inspo1': ['"Always deliver more than expected." -Larry Page, Google co-founder', 
+    '"Stay self-funded as long as possible." -Garrett Camp, Stumbleupon co-founder ',
     '"Ideas are easy. Implementation is hard." -Guy Kawasaki, AllTop co-founder', 
-    'D', 'E'];
-const inspo2 = [1, 2, 3, 4, 5];
-const inspo3 = ['Billionaires Build', 'How to Think for Yourself', 'Early Work', 
+    '"Your most unhappy customers are your greatest source of learning." -Bill Gates, Microsoft co-founer', 
+    '"Make mistakes faster." -Andrew Grove, Intel co-founder', 
+    '"Stay hungry. And bootstrap." -Rob Kalin, Etsy co-founder',
+    '"Learn early. Learn often." -Drew Houston, Dropbox co-founder',
+    '"Every day spent not improving your product is a wasted day." -Joel Spolsky, Stack Exchange CEO',
+    '"Timing, perseverance, and ten years of trying will eventually make you look like an overnight success." -Biz Stone, Twitter co-founer'],
+    'inspo2': [1, 2, 3, 4, 5],
+    'inspo3': ['Billionaires Build', 'How to Think for Yourself', 'Early Work', 
     "What Doesn't Seem Like Work?", 'Before the Startup', "Do Things that Don't Scale",
     "The Hardware Renaissance", 'Startup = Growth', 'A Word to be Resourceful',
-    'Why Startup Hubs Work', 'Founder Control', 'Keep Your Identity Small', 'Six Principles for Making New Things'];
-
-const printRandomMessage = (list1, list2, list3) => {
-    let selection1 = Math.floor((Math.random() * list1.length));
-    let selection2 = Math.floor((Math.random() * list2.length));
-    let selection3 = Math.floor((Math.random() * list3.length));
-    console.log(`Remember: ${list1[selection1]}`);
-    console.log(list2[selection2]);
-    console.log(`For insight read "${list3[selection3]}" by Paul Graham`);
+    'Why Startup Hubs Work', 'Founder Control', 'Keep Your Identity Small', 'Six Principles for Making New Things']
 }
 
-printRandomMessage(inspo1, inspo2, inspo3);
+const printRandomMessage = obj => {
+    let selection1 = Math.floor((Math.random() * obj['inspo1'].length));
+    let selection2 = Math.floor((Math.random() * obj['inspo2'].length));
+    let selection3 = Math.floor((Math.random() * obj['inspo3'].length));
+    console.log(`Remember: ${obj['inspo1'][selection1]}`);
+    console.log(obj['inspo2'][selection2]);
+    console.log(`For insight read "${obj['inspo3'][selection3]}" by Paul Graham`);
+}
+
+printRandomMessage(messageComponents);
