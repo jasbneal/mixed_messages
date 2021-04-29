70cb627c3707764e457077604983cd9e9c6ff37d
jasbneal <75139330+jasbneal@users.noreply.github.com>
2021-04-29 13:51:51 -0400

    inspo3 array = P.G. writings

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ for a founder when it runs. */
 
 const inspo1 = ['A', 'B', 'C', 'D', 'E'];
 const inspo2 = [1, 2, 3, 4, 5];
-const inspo3 = ['@', '$', '#', '^', '+'];
+const inspo3 = ['Billionaires Build', 'How to Think for Yourself', 'Early Work', 
+    "What Doesn't Seem Like Work?", 'Before the Startup', "Do Things that Don't Scale",
+    "The Hardware Renaissance", 'Startup = Growth', 'A Word to be Resourceful',
+    'Why Startup Hubs Work', 'Founder Control', 'Keep Your Identity Small', 'Six Principles for Making New Things'];
 
 const printRandomMessage = (list1, list2, list3) => {
     let selection1 = Math.floor((Math.random() * list1.length));
@@ -11,7 +14,7 @@ const printRandomMessage = (list1, list2, list3) => {
     let selection3 = Math.floor((Math.random() * list3.length));
     console.log(list1[selection1]);
     console.log(list2[selection2]);
-    console.log(list3[selection3]);
+    console.log(`For insight read "${list3[selection3]}" by Paul Graham`);
 }
 
 printRandomMessage(inspo1, inspo2, inspo3);
